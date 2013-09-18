f5ec931ed6c86f418f4a9edda372fc54c814c5e2
Zack Katz <zack@katzwebservices.com>
2013-09-17 18:32:37 -0600

    Fix path
    BIGCOMMERCE_PLUGIN_FILE

--- trunk/lib/presstrends.php
+++ trunk/lib/presstrends.php
@@ -29,7 +29,7 @@ function bigcommerce_presstrends_plugin() {
 		}
 
 		// CHANGE __FILE__ PATH IF LOCATED OUTSIDE MAIN PLUGIN FILE
-		$plugin_data         = get_plugin_data( __FILE__ );
+		$plugin_data         = get_plugin_data( BIGCOMMERCE_PLUGIN_FILE );
 		$posts_with_comments = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type='post' AND comment_count > 0" );
 		$data                = array(
 		    'url'             => stripslashes( str_replace( array( 'http://', '/', ':' ), '', site_url() ) ),
